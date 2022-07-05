8c112c8cf8a6d44e3e0a0eb0ccbd58d2260b71dc
Renato Lomba <rntlomba@gmail.com>
2022-07-04 21:35:37 -0300

    feat: :zap: Prisma config
    Platscale and Prisma ORM integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,38 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 
-const Home: NextPage = () => {
-  return <h1 className="font-bold text-blue-500">Hello world</h1>;
+import { PollQuestion } from '@prisma/client';
+
+import { prisma } from '../db/prisma';
+
+interface IHomeProps {
+  questions: string;
+}
+
+const Home: NextPage<IHomeProps> = ({ questions }) => {
+  const parsedQuestions = JSON.parse(questions) as PollQuestion[];
+
+  return (
+    <div>
+      <h1 className="font-bold text-blue-500">Hello world Tailwind CSS</h1>
+
+      <ul>
+        {parsedQuestions.map((question) => (
+          <li key={question.id}>{question.question}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
+const getServerSideProps: GetServerSideProps = async () => {
+  const questions = await prisma.pollQuestion.findMany();
+
+  return {
+    props: {
+      questions: JSON.stringify(questions),
+    },
+  };
+};
+
+export { getServerSideProps };
 export default Home;
