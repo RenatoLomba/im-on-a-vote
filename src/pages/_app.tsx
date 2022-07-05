2ea9a2e4153408078de1942ce62a18537a536118
Renato Lomba <rntlomba@gmail.com>
2022-07-05 20:06:12 -0300

    config url for non-ssr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,9 +15,17 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : 'http://localhost:3000/api/trpc';
+    // const url = process.env.VERCEL_URL
+    //   ? `https://${process.env.VERCEL_URL}/api/trpc`
+    //   : 'http://localhost:3000/api/trpc';
+
+    let url = 'http://localhost:3000/api/trpc';
+
+    if (process.env.VERCEL_URL) {
+      url = `https://${process.env.VERCEL_URL}/api/trpc`;
+    } else if (process.env.NODE_ENV === 'production') {
+      url = '/api/trpc';
+    }
 
     return {
       url,
