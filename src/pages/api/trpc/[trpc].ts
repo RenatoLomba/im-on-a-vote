5ec8b2b0d17fc39975d4d89fd697f8ffc6eb4d7a
Renato Lomba <rntlomba@gmail.com>
2022-07-05 19:06:42 -0300

    Trpc config

---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,24 @@
+import { z } from 'zod';
+
+import * as trpc from '@trpc/server';
+import * as trpcNext from '@trpc/server/adapters/next';
+
+export const appRouter = trpc.router().query('hello', {
+  input: z
+    .object({
+      text: z.string().nullish(),
+    })
+    .nullish(),
+  resolve({ input }) {
+    return {
+      greeting: `hello ${input?.text ?? 'world'}`,
+    };
+  },
+});
+
+export type AppRouter = typeof appRouter;
+
+export default trpcNext.createNextApiHandler({
+  router: appRouter,
+  createContext: () => null,
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,21 @@
-import type { GetServerSideProps, NextPage } from 'next';
+import type { NextPage } from 'next';
 
-import { PollQuestion } from '@prisma/client';
+import { trpc } from '../utils/trpc';
 
-import { prisma } from '../db/prisma';
+const Home: NextPage = () => {
+  const hello = trpc.useQuery(['hello', { text: 'Renato' }]);
 
-interface IHomeProps {
-  questions: string;
-}
-
-const Home: NextPage<IHomeProps> = ({ questions }) => {
-  const parsedQuestions = JSON.parse(questions) as PollQuestion[];
+  if (hello.isLoading || !hello.data) {
+    return <div>...Loading</div>;
+  }
 
   return (
     <div>
       <h1 className="font-bold text-blue-500">Hello world Tailwind CSS</h1>
 
-      <ul>
-        {parsedQuestions.map((question) => (
-          <li key={question.id}>{question.question}</li>
-        ))}
-      </ul>
+      <p>{hello.data.greeting}</p>
     </div>
   );
 };
 
-const getServerSideProps: GetServerSideProps = async () => {
-  const questions = await prisma.pollQuestion.findMany();
-
-  return {
-    props: {
-      questions: JSON.stringify(questions),
-    },
-  };
-};
-
-export { getServerSideProps };
 export default Home;
