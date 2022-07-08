9f68c0e7097952e17aa164d9aa76faf174f01072
Renato Lomba <rntlomba@gmail.com>
2022-07-07 21:00:05 -0300

    Refactor question-details component

---
+++ src/client/components/question-details.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react';
+
+import { trpc } from '../utils/trpc';
+
+export const QuestionDetails: FC<{ slug: string }> = ({ slug }) => {
+  const { data, isLoading, isError, error } = trpc.useQuery([
+    'questions.getBySlug',
+    { slug },
+  ]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError && error) {
+    return (
+      <div className="text-red-500 text-xl font-bold">{error.message}</div>
+    );
+  }
+
+  if (!data || !data.question) {
+    return <div>Question not found</div>;
+  }
+
+  const { question, isOwner } = data;
+
+  return (
+    <div className="p-8 flex flex-col">
+      {isOwner && (
+        <div className="m-4 bg-red-700 rounded-md p-3 text-white">
+          You made this!
+        </div>
+      )}
+
+      <h1 className="text-2xl font-bold">{question.title}</h1>
+
+      <ul>
+        {(question.options as string[])?.map((option) => (
+          <li key={option}>{option}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- src/pages/question/[slug].tsx
+++ src/pages/question/[slug].tsx
@@ -1,50 +1,15 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
-import { trpc } from '../../client/utils/trpc';
+import { QuestionDetails } from '../../client/components/question-details';
 
 const QuestionPage: NextPage = () => {
   const router = useRouter();
   const { slug } = router.query;
 
-  const { data, isLoading, isError, error } = trpc.useQuery([
-    'questions.getBySlug',
-    { slug: slug! as string },
-  ]);
+  if (!slug || Array.isArray(slug)) return <div>No slug</div>;
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isError && error) {
-    return (
-      <div className="text-red-500 text-xl font-bold">{error.message}</div>
-    );
-  }
-
-  if (!data || !data.question) {
-    return <div>Question not found</div>;
-  }
-
-  const { question, isOwner } = data;
-
-  return (
-    <div className="p-8 flex flex-col">
-      {isOwner && (
-        <div className="m-4 bg-red-700 rounded-md p-3 text-white">
-          You made this!
-        </div>
-      )}
-
-      <h1 className="text-2xl font-bold">{question.title}</h1>
-
-      <ul>
-        {(question.options as string[])?.map((option) => (
-          <li key={option}>{option}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <QuestionDetails slug={slug} />;
 };
 
 export default QuestionPage;
